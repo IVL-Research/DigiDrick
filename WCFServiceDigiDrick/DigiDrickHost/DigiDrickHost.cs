8d2d34a96775c0a6305e7b2625611967d9a7b8ba
IVL-Research <38903064+IVL-Research@users.noreply.github.com>
2018-11-15 10:55:00 +0100

    Add files via upload

---
+++ WCFServiceDigiDrick/DigiDrickHost/DigiDrickHost.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace IVL.DigiDrick.Host
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using IVL.DigiDrick.ModelLib;
+    using System.ServiceModel;
+    /// <summary>
+    /// Host program for the DigiDrick services. Displays error messages.
+    /// </summary>
+    public class DigiDrickHost
+    {
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            using (ServiceHost serviceHost = new ServiceHost(typeof(DigiDrickModel)))
+            {
+                serviceHost.Open();
+
+                Console.Clear();
+                Console.Title ="****** Console Based WCF DigiDrick Host *****";
+
+                // Keep the service running until Enter key is pressed.
+                Console.WriteLine("The DigiDrick service is ready.");
+                Console.WriteLine("Press the Enter key to terminate service.");
+                Console.WriteLine(" ------------ Logg ------------ ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
